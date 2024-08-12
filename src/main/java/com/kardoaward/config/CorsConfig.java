a3d1a3228a4443cfc34556365bb4bfd2773cd575
Evgeny <e-walker@yandex.ru>
2024-08-12 15:47:59 +0300

    fix: CORS

--- src/main/java/com/kardoaward/config/CorsConfig.java
+++ src/main/java/com/kardoaward/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://51.250.35.168","http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
